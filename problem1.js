2a3819f0378e1a4a453e537ccd22b714d69c8af5
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2026-07-29 23:06:21 +0600

    Problem 1 done

--- problem1.js
+++ problem1.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+// Problem 1 — Match Winner //
+
+function matchWinner(teamAGoals, teamBGoals) {
+  if (typeof teamAGoals !== "number" || typeof teamBGoals !== "number") {
+    return "Invalid"; // Input Type Check //
+  }
+  if (teamAGoals > teamBGoals) {
+    return "Team A Won";
+  } else if (teamAGoals < teamBGoals) {
+    return "Team B Won";
+  } else {
+    return "Draw";
+  }
+};
