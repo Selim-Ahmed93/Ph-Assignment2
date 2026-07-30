bc0a397bfb5404573bbe33e90842382b2105ecef
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2026-07-30 21:59:07 +0600

    Problem 4 solved

--- problem2.js
+++ problem2.js
@@ -16,3 +16,4 @@ function isElevatorSafe(weights) {
   }
 };
 
+
---
+++ problem3.js
@@ -0,0 +1,16 @@
+// Problem 3 — AI Token Cost Calculator
+
+function calculateAiCost(tokensUsed) {
+  if (typeof tokensUsed !== "number" || tokensUsed < 0) {
+    return "Invalid"; //Input Type Check Number or Negative number and String
+  }
+
+  if (tokensUsed <= 500) {
+    return 0;
+  }
+  let freeToken = tokensUsed - 500;
+  let costing = Math.floor(freeToken / 100) * 5;
+
+  return costing;
+};
+
