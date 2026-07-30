83cb16e501764dd32866510e724be8a62815e0e8
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2026-07-30 21:10:18 +0600

    Problem 2 Solve

---
+++ problem2.js
@@ -0,0 +1,18 @@
+// Problem Statement 2 
+
+function isElevatorSafe(weights) {
+  if (!Array.isArray(weights)) {
+    return "Invalid"; // Input Type Check //
+  }
+  let sum = 0;
+
+ for (let weight of weights) {
+    sum = sum + weight; // loop in Array get total weights
+  }
+  if (sum < 400) {
+    return "Safe";
+  } else {
+    return "Unsafe";
+  }
+};
+
